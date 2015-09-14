4100a1326a86a7024b604bca50d365a63e62346c
JellyKing <curriedmc@gmail.com>
2015-09-14 15:30:26 +0100

    Initial commit

---
+++ Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    public class Character
+    {
+        //ang aking character class.
+        //may generic katangian (atribute)
+        /*
+         * Color
+         * Name
+         * Age
+         * Gender
+         * 
+         */
+
+        //easier to set with properties, than lots of methods. also looks prettier like this
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Character(string color, string name, int age, string gender)
+        {
+            //this needs to have the same name as the public class declaration. this is ran when you instantiate the class object. From here, all variables should be set.
+            Color = color;
+            Name = name;
+            Age = age;
+            Gender = gender;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate object.
+            //need to add in variables too.
+
+            string Color = "White";
+            string Name = "Sasuke";
+            int Age = 15;
+            string Gender = "Male";
+
+            Character Sasuke = new Character(Color, Name, Age, Gender);//instantiate object, then pass in variables white have been decided on.
+            //now sasuke has been created. This is a generic character and you could fill an array full of random values
+            
+        }
+    }
+}
